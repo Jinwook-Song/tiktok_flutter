f3c0357322c018fff890088f8025ede67fa390dd
jinwook song <wlsdnr129@naver.com>
2023-01-18 21:10:02 +0900

    fix: file path

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_flutter/constants/sizes.dart';
-import 'package:tiktok_flutter/features/authentication/widgets/email_screen.dart';
+import 'package:tiktok_flutter/features/authentication/email_screen.dart';
 
 enum Destination { email, apple }
 
