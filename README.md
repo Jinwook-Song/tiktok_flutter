32ce62c44e7e27ab0b39ed6c55c68977d43f6095
jinwook song <wlsdnr129@naver.com>
2023-01-18 22:02:20 +0900

    feat: input & TextEditingController

--- README.md
+++ README.md
@@ -24,3 +24,123 @@ _widthFactor_
 ---
 
 ### Auth button
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_flutter/constants/sizes.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+  final FaIcon icon;
+
+  const AuthButton({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        padding: const EdgeInsets.all(
+          Sizes.size14,
+        ),
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.grey.shade300,
+          width: Sizes.size1,
+        )),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+```
+
+### TextField
+
+```dart
+TextField(
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(
+                  color: Colors.black26,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black12,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black12,
+                  ),
+                ),
+              ),
+            ),
+```
+
+### TextEditingController
+
+변화를 감지할 수 있음
+
+```dart
+final TextEditingController _usernameController = TextEditingController();
+
+  String _username = '';
+  bool isValid = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+      isValid = _username.length >= 2 ? true : false;
+    });
+  }
+```
+
+### AnimatedContainer
+
+```dart
+AnimatedContainer(
+                duration: const Duration(milliseconds: 150),
+                padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+                decoration: BoxDecoration(
+                    color: isValid
+                        ? Theme.of(context).primaryColor
+                        : Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(Sizes.size5)),
+                child: Text(
+                  'Next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: isValid ? Colors.white : Colors.black38,
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size16),
+                ),
+              ),
+```
--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -2,8 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/constants/gaps.dart';
 import 'package:tiktok_flutter/constants/sizes.dart';
 
-class EmailScreen extends StatelessWidget {
+class EmailScreen extends StatefulWidget {
   const EmailScreen({super.key});
+
+  @override
+  State<EmailScreen> createState() => _EmailScreenState();
+}
+
+class _EmailScreenState extends State<EmailScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = '';
+  bool isValid = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+      isValid = _username.length >= 2 ? true : false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +41,9 @@ class EmailScreen extends StatelessWidget {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+          children: [
             Gaps.v40,
-            Text(
+            const Text(
               'Create username',
               style: TextStyle(
                 fontSize: Sizes.size24,
@@ -29,13 +51,55 @@ class EmailScreen extends StatelessWidget {
               ),
             ),
             Gaps.v10,
-            Text(
+            const Text(
               'You can always change this later.',
               style: TextStyle(
                 fontSize: Sizes.size16,
                 color: Colors.black54,
               ),
             ),
+            Gaps.v40,
+            TextField(
+              controller: _usernameController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(
+                  color: Colors.black26,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black12,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black12,
+                  ),
+                ),
+              ),
+            ),
+            Gaps.v32,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 150),
+                padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+                decoration: BoxDecoration(
+                    color: isValid
+                        ? Theme.of(context).primaryColor
+                        : Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(Sizes.size5)),
+                child: Text(
+                  'Next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: isValid ? Colors.white : Colors.black38,
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size16),
+                ),
+              ),
+            )
           ],
         ),
       ),
