e153ac0a62d3daf466dc493e952757b6e7d24d59
jinwook song <wlsdnr129@naver.com>
2023-01-29 21:40:56 +0900

    style: interests

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/constants/gaps.dart';
 import 'package:tiktok_flutter/constants/sizes.dart';
 import 'package:tiktok_flutter/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_flutter/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -14,62 +15,80 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   Map<String, String> formData = {};
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   void _onSubmitTap() {
     // trigger validation
     bool? validated = _formKey.currentState?.validate();
     if (validated == true) {
       _formKey.currentState!.save();
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const InterestsScreen(),
+        ),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log in'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Log in'),
         ),
-        child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Gaps.v28,
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Email'),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['email'] = newValue;
-                    }
-                  },
-                ),
-                Gaps.v16,
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Password'),
-                  validator: (value) {
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      formData['password'] = newValue;
-                    }
-                  },
-                ),
-                Gaps.v28,
-                GestureDetector(
-                  onTap: _onSubmitTap,
-                  child: const FormButton(
-                    isValid: true,
-                    text: 'Log in',
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Gaps.v28,
+                  TextFormField(
+                    decoration: const InputDecoration(hintText: 'Email'),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Please write your email";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData['email'] = newValue;
+                      }
+                    },
                   ),
-                )
-              ],
-            )),
+                  Gaps.v16,
+                  TextFormField(
+                    decoration: const InputDecoration(hintText: 'Password'),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "Please write your password";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData['password'] = newValue;
+                      }
+                    },
+                  ),
+                  Gaps.v28,
+                  GestureDetector(
+                    onTap: _onSubmitTap,
+                    child: const FormButton(
+                      isValid: true,
+                      text: 'Log in',
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
