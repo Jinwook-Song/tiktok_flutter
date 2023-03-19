a159247b28d1e947cd901963b58c6dabf9ce135a
jinwook song <wlsdnr129@naver.com>
2023-03-19 18:10:37 +0900

    feat: animated list

--- README.md
+++ README.md
@@ -1646,3 +1646,102 @@ if (_showBarrier)
               onDismiss: _toggleAnimations,
             ),
 ```
+
+### AnimatedList
+
+_itemBuilder_: (_context_, _index_, _animation_) {}
+
+Animation<double> animation
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_flutter/constants/sizes.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Direct messages'),
+        elevation: 1,
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const FaIcon(
+              FontAwesomeIcons.plus,
+              size: Sizes.size20,
+            ),
+          ),
+        ],
+      ),
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                key: UniqueKey(),
+                leading: const CircleAvatar(
+                  radius: Sizes.size32,
+                  foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/78011042?v=4',
+                  ),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'AntonioBM $index',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '2:16 PM',
+                      style: TextStyle(
+                        fontSize: Sizes.size12,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text(
+                  'Say hi to AntonioBM',
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+```
--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,22 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +34,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
         elevation: 1,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
               size: Sizes.size20,
@@ -26,40 +42,48 @@ class _ChatsScreenState extends State<ChatsScreen> {
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: Sizes.size32,
-              foregroundImage: NetworkImage(
-                'https://avatars.githubusercontent.com/u/78011042?v=4',
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                key: UniqueKey(),
+                leading: const CircleAvatar(
+                  radius: Sizes.size32,
+                  foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/78011042?v=4',
+                  ),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'AntonioBM $index',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '2:16 PM',
+                      style: TextStyle(
+                        fontSize: Sizes.size12,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text(
+                  'Say hi to AntonioBM',
+                ),
               ),
             ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  'AntonioBM',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  '2:16 PM',
-                  style: TextStyle(
-                    fontSize: Sizes.size12,
-                    color: Colors.grey.shade500,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: const Text(
-              'Say hi to AntonioBM',
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
